1fcb35a13298b2d27bbf910d4321cfcbd19493ef
z3sh4n <ghaniwalamohmmad@gmail.com>
2025-09-11 19:39:44 +0530

    started project, did load docs and made vector store

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from pdf-rag!")
+from langchain_chroma import Chroma
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_community.document_loaders import DirectoryLoader
+from dotenv import load_dotenv
+
+load_dotenv()
+
+gemini_embeddings = GoogleGenerativeAIEmbeddings(model="gemini-embedding-001")
+
+directory_loader = DirectoryLoader("./docs", glob="*.pdf")
 
 
-if __name__ == "__main__":
-    main()
+vector_store = Chroma(
+    collection_name='docs',
+    embeddings=gemini_embeddings,
+    persist_directory='./chroma_db',
+)
